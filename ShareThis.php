2250fba722a01d02aed3cee7dfe7ffbc8d8acd94
Suki <suki@missallsunday.com>
2013-02-21 08:19:09 -0600

    No need for this at elast on recent jquery versions
    Signed-off-by: Suki <suki@missallsunday.com>

--- ShareThis.php
+++ ShareThis.php
@@ -326,9 +326,9 @@ class ShareThis
 					jQuery("#msg_'. $this->msgID .'").hoverIntent(function()
 					{
 					jQuery("#msg_'. $this->msgID .'").css("overflow-y", "hidden");
-					jQuery(".sharethis_'. $this->msgID .'").delay(100).fadeIn();
+					jQuery(".sharethis_'. $this->msgID .'").fadeIn();
 					},function(){
-							jQuery(".sharethis_'. $this->msgID .'").delay(300).fadeOut();
+							jQuery(".sharethis_'. $this->msgID .'").fadeOut();
 						});
 				});
 			});
